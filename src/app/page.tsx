7889bb536fba2be09c2063aaa13ddbaa43d20f38
Black Wolf Team <47728533+blackwolfinc@users.noreply.github.com>
2024-03-30 09:40:55 +0700

    fix styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,23 @@ import { BackgroundAnimate } from "@/components/BackgroundAnimate/BackgroundAnim
 
 export default function Home() {
   return (
-    <main className=" w-screen h-screen relative bg-gradient-to-br from-[#1f1f1f50] via-[#4d537a99] to-[#080707]">
+    <main className=" w-screen h-screen relative bg-gradient-to-br from-[#1f1f1f50] via-[#45464e99] to-[#080707]">
       <div className="background-main w-screen h-screen absolute z-20 "></div>
       <div className="background-second w-screen h-screen absolute z-20 "></div>
+      <div className="background-third w-screen h-screen absolute z-20 "></div>
       <BackgroundAnimate />
-      <div className="flex flex-col w-full h-full  relative  justify-center  items-center  scale-50">
+      <div className="flex flex-col w-full h-full  relative  justify-center  items-center  scale-75">
         <div className="mt-[-10rem] flex justify-center  content-center self-center items-center dropLogo ">
           <div className=" absolute right-0 top-0 opacity-70">
-          <Image alt="" height={350} width={450} src={Claw}  className="mt-[-3rem] opacity-45 shad"/>
+            <Image
+              alt=""
+              height={350}
+              width={450}
+              src={Claw}
+              className="mt-[-3rem] opacity-45 shad"
+            />
           </div>
-          <Image alt="" height={300} width={550} src={wolf} className="z-20" />
+          <Image alt="" height={300} width={550} src={wolf} className="z-20 " />
         </div>
       </div>
     </main>
