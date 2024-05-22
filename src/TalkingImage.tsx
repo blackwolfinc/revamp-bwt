a408a64d4d8a6a84df34aa12de0a6007a1820c56
Black Wolf Team <47728533+blackwolfinc@users.noreply.github.com>
2024-05-22 16:43:53 +0700

    fix

--- src/TalkingImage.tsx
+++ src/TalkingImage.tsx
@@ -1,8 +1,7 @@
+"use client"
 import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
 
 const TalkingImage = ({ currentWord }:any) => {
-  const [animationClass, setAnimationClass] = useState('');
   console.log(currentWord, "currentWord")
   const wordToImageMapping:any = {
     'a': 'a.webp',
@@ -36,12 +35,7 @@ const TalkingImage = ({ currentWord }:any) => {
 
   const mouthMovement = wordToImageMapping[currentWord] || wordToImageMapping['default'];
 
-  useEffect(() => {
-    setAnimationClass('animate');
-    const timeoutId = setTimeout(() => setAnimationClass(''), 100); // Reset animation class after 200ms
-    return () => clearTimeout(timeoutId);
-  }, [currentWord]);
-
+ 
   return (
     <div className="flex justify-center mt-4 z-50  relative overflow-hidden mr-[-2rem]">
       <Image
@@ -49,9 +43,9 @@ const TalkingImage = ({ currentWord }:any) => {
         alt="Talking Mouth"
         height={130}
         width={130}
-        quality={1}
+        quality={10}
         priority
-        className={`w-24 h-24 ${animationClass} border-[4px] rounded-3xl `} // Apply animation class
+        className={`w-24 h-24 border-[4px] rounded-3xl `} // Apply animation class
       />
     </div>
   );
